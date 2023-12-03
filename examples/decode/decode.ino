0b614cf4ae4ddb2b450bf2a5a46382d1c7ded1fe
gb-123-git <64229388+gb-123-git@users.noreply.github.com>
2023-12-03 18:59:50 +0000

    Changed NameSpace for better compatibility

--- examples/decode/decode.ino
+++ examples/decode/decode.ino
@@ -5,8 +5,8 @@ void setup(){
     while(!Serial);
 
     const char input[] = "F3c7EYKkxMgnvO0nB8FWVw==";
-    uint8_t output[base64::decodeLength(input)];
-    base64::decode(input, output);
+    uint8_t output[data_base64::decodeLength(input)];
+    data_base64::decode(input, output);
 
     Serial.println((const char*)output);
 }
--- examples/encode/encode.ino
+++ examples/encode/encode.ino
@@ -6,8 +6,8 @@ void setup(){
 
     const uint8_t input[] = {0x17, 0x77, 0x3B, 0x11, 0x82, 0xA4, 0xC4, 0xC8};
     auto inputLength = sizeof(input);
-    char output[base64::encodeLength(inputLength)];
-    base64::encode(input, inputLength, output);
+    char output[data_base64::encodeLength(inputLength)];
+    data_base64::encode(input, inputLength, output);
 
     Serial.println(output);
 }
